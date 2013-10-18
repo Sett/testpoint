25f29e324c36da22b3c01c28b53325bcacbd2db9
Se# <support@funcraft.ru>
2013-10-18 13:35:52 +0400

    Update Analyse.php

--- Analyse.php
+++ Analyse.php
@@ -2,13 +2,24 @@
 
 trait Analyse
 {
-  public function analyseOk()
+  public function analyseOk($result)
   {
-    // todo
+      preg_match('/.+\((\d+)\s.+/', $result, $pockets);// get the count of tests
+      $points = $pockets[1];// for each test gain 1 point
+      return ['OK' => true, 'data' => $points];
   }
   
-  public function analyseFail()
+  public function analyseFail($result)
   {
-    // todo
+      preg_match('/Tests:\s(\d+).+(Failures:\s(\d))*/', $result, $pockets);// get the count of tests and failures
+      $totalPoints = $pockets[1];// the count of tests
+      $losePoints  = $pockets[2];
+      return ['OK' => false, 
+              'data' => 
+                  [
+                      'total' => $totalPoints, 
+                      'lose' => $losePoints
+                  ]
+              ];
   }
 }
