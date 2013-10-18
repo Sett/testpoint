931ff289bc6361c667344ebb3cabb31d751da8dd
Se# <support@funcraft.ru>
2013-10-18 13:37:04 +0400

    Update TestPoint.php

--- TestPoint.php
+++ TestPoint.php
@@ -60,24 +60,9 @@ class TestPoint
     public function analyse($result)
     {
         if(strpos($result, 'OK') !== false)
-        {
-            preg_match('/.+\((\d+)\s.+/', $result, $pockets);// get the count of tests
-            $points = $pockets[1];// for each test gain 1 point
-            return ['OK' => true, 'data' => $points];
-        }
+            return $this->analyseOk($result);
         else
-        {
-            preg_match('/Tests:\s(\d+).+(Failures:\s(\d))*/', $result, $pockets);// get the count of tests and failures
-            $totalPoints = $pockets[1];// the count of tests
-            $losePoints  = $pockets[2];
-            return ['OK' => false, 
-                    'data' => 
-                        [
-                            'total' => $totalPoints, 
-                            'lose' => $losePoints
-                        ]
-                    ];
-        }
+            return $this->analyseFail($result);
     }
 
     /**
