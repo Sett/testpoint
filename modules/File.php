cd17474ab031881112fb8113351f773a3f4053e6
Se# <support@funcraft.ru>
2013-10-22 15:09:09 +0400

    Create File.php

---
+++ modules/File.php
@@ -0,0 +1,28 @@
+<?php
+
+trait File.php
+{
+  public function getFiles($directory, $fileMask, $lookInSubdirs = false)
+  {
+    $directories = [];
+    
+    if($lookInSubdirs)
+    {
+      $baseDir =$directory;
+      while($dirs = glob($baseDir . '/*', GLOB_ONLYDIR))
+      {
+          $directories = array_merge($directories, $dirs);
+          $baseDir .= '/*';
+      }
+    }
+    else
+      $directories[] = $directory
+    
+    $files = [];
+    
+    foreach($directories as $dir)
+        $files = array_merge($files, glob($dir . $fileMask));
+        
+    return $files;
+  }
+}
