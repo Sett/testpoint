e713bc30a052c068cf66f09e187ccf8bb38e0983
Se# <support@funcraft.ru>
2013-10-22 15:10:35 +0400

    Update Test.php

--- modules/Test.php
+++ modules/Test.php
@@ -1,8 +1,16 @@
 <?php
 
+require_once 'File.php';
+
 trait Test
 {
+  /**
+   * For working with files
+   */
+  use File;
+  
   public function getTests($directory)
   {
+    return $this->getFiles($directory, '/*.php', true);
   }
 }
