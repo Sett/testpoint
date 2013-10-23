8da91b852e16ef8d08e8c7b600a89646d26c12d5
Se# <support@funcraft.ru>
2013-10-23 12:19:57 +0400

    Update PHPUnit.php

--- modules/PHPUnit.php
+++ modules/PHPUnit.php
@@ -7,10 +7,6 @@ trait PHPUnit
   public function exec($test)
   {
     exec('phpunit ' . $test, $output);
-        
-    if($this->logExec)
-        file_put_contents($this->execLog, json_encode($output));
-    
     return $output;
   }
 }
