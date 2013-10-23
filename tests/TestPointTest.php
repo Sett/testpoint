b3e2151b8578dbab534895046e7353fb0686dc05
Se# <support@funcraft.ru>
2013-10-23 13:10:10 +0400

    Update TestPointTest.php

--- tests/TestPointTest.php
+++ tests/TestPointTest.php
@@ -3,4 +3,23 @@
 class TestPointTest extends PHPUnit_Framework_TestCase
 {
   // todo: write own test (((:
+  /**
+   * @dataProvider testProvider
+   */
+  public function testTP($player, $result)
+  {
+    $testPoint = new TestPoint($player, ['TestExample.php']);
+    $testPoint->log($player, $result);
+  }
+  
+  public function testProvider()
+  {
+    return [
+      ['empty', []],
+      ['okTrue', ['OK' => true]],
+      ['okFalse', ['OK' => false]],
+      ['fullDataOK', ['OK' => true, 'data' => 5]],
+      ['fullDataFail', ['OK' => true, 'data' => ['total' => 4, 'lose' => 1]]]
+    ];
+  }
 }
