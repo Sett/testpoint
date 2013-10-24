9a46ff6e941648c5e2618f1b292965c2c85f13ca
Se# <support@funcraft.ru>
2013-10-24 14:32:04 +0400

    Update Analyse.php

--- modules/PHPUnit/Analyse.php
+++ modules/PHPUnit/Analyse.php
@@ -23,9 +23,11 @@ trait PHPUnit_Analyse
   
   public function analyseFail($result)
   {
-      preg_match('/Tests:\s(\d+).+(Failures:\s(\d))*/', $result, $pockets);// get the count of tests and failures
+      $pattern = '/Tests:\s(\d+),\s*(Assertions:\s*(\d+))*,\s*(Errors:\s*(\d+))*\s*(Failures:\s*(\d+))*/';
+      preg_match($pattern, $result, $pockets);// get the count of tests and failures
       $totalPoints = isset($pockets[1]) ? $pockets[1] : 0;// the count of tests
-      $losePoints  = isset($pockets[2]) ? $pockets[2] : 0;
+      $losePoints  = isset($pockets[5]) ? $pockets[5] : 0;// the count of errors
+      $losePoints  += isset($pockets[7]) ? $pockets[7] : 0;// the count of failures
       return ['OK' => false, 
               'data' => 
                   [
