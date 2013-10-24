35f27978b68c2746488fb4937442cc2d74b230b7
Se# <support@funcraft.ru>
2013-10-24 14:38:45 +0400

    Update Mode.php

--- modules/Mode.php
+++ modules/Mode.php
@@ -1,4 +1,21 @@
 <?php
-
+/**
+ * operate mode
+ */
 trait Mode
-{}
+{
+  public $mode = 'talking';
+  
+  public $talkTypes = [
+    'h1' => '===== $talk =====',
+    'h2' => '=== $talk ===',
+    'endOfEpisode' => '$talk' . "\n\n",
+    'default' => '$talk'
+  ];
+  
+  public function say($text, $type)
+  {
+    if(($this->mode == 'talking') && isset($this->talkTypes[$type]))
+      echo str_replace('$talk', $text, $this->talkTypes[$type]);
+  }
+}
