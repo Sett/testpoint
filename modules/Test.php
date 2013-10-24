a2f8b82e19280b3c166040b9b036103058f83392
Se# <support@funcraft.ru>
2013-10-24 15:04:11 +0400

    Update Test.php

--- modules/Test.php
+++ modules/Test.php
@@ -11,7 +11,18 @@ trait Test
   
   public function getTests($directory)
   {
-    $this->say('Start looking tests in', 'startOfEpisode');
-    return $this->getFiles($directory, '/*.php', true);
+    if(is_array($directory))
+    {
+        $this->say('Tests for playing: ' . implode("\n", $directory));
+        $tests = $directory;
+    }
+    else
+    {
+        $this->say('Start looking tests in', 'startOfEpisode');
+        $tests = $this->getFiles($directory, '/*.php', true);
+    }
+
+
+    return $tests;
   }
 }
