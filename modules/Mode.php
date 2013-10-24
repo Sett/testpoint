16298f1a883164592035806300bb3178ce6e0404
Se# <support@funcraft.ru>
2013-10-24 14:59:35 +0400

    Update Mode.php

--- modules/Mode.php
+++ modules/Mode.php
@@ -7,16 +7,16 @@ trait Mode
   public $mode = 'talk';
   
   public $talkTypes = [
-    'h1' => "\n=====| $talk |=====\n",
-    'h2' => "\n===| $talk |===\n",
-    'startOfEpisode' => "\n$talk\n",
-    'endOfEpisode' => "$talk\n\n",
-    'default' => "$talk\n"
+    'h1' => "\n=====| #talk |=====\n",
+    'h2' => "\n===| #talk |===\n",
+    'startOfEpisode' => "\n#talk\n",
+    'endOfEpisode' => "#talk\n\n",
+    'default' => "#talk\n"
   ];
   
   public function say($text, $type = 'default')
   {
     if(($this->mode == 'talk') && isset($this->talkTypes[$type]))
-      echo str_replace('$talk', $text, $this->talkTypes[$type]);
+      echo str_replace('#talk', $text, $this->talkTypes[$type]);
   }
 }
