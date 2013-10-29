282c60e5a5457f761a4eacb33c32de1d68741164
Se# <support@funcraft.ru>
2013-10-29 15:53:30 +0300

    Create Self.php

---
+++ modules/PHPUnit/Self.php
@@ -0,0 +1,8 @@
+<?php
+// --loader <loader>         TestSuiteLoader implementation to use.
+// --printer <printer>       TestSuiteListener implementation to use.
+// --repeat <times>          Runs the test(s) repeatedly.
+trait PHPUnit_Self
+{
+
+}
