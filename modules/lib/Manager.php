dfd6e05cef15f5dd7c4d7e7ef3b4a40fe76022f4
Se# <support@funcraft.ru>
2013-10-29 12:31:16 +0300

    Update Manager.php

--- modules/lib/Manager.php
+++ modules/lib/Manager.php
@@ -1,8 +1,48 @@
 <?php
 /**
- * For choosing wich modules needs for the TestPoint
+ * For choosing which modules needs for the TestPoint
  */
 class TestPoint_Manager
 {
-  // todo
+    /**
+     * @param string $resultClassName
+     * @param string $cfgPath
+     * @param string $traitBasePath
+     */
+    public static function compile($resultClassName = '', $cfgPath = '', $traitBasePath = '/')
+    {
+        $cfg = json_decode(file_get_contents($cfgPath), true);
+        $traits = isset($cfg['traits']) ? $cfg['traits'] : [];
+
+        $use = 'use ';
+        $require = '';
+        $traitsCount = count($traits)-1;
+
+        foreach($traits as $index => $traitName)
+        {
+            $quot = ($index == $traitsCount) ? ';' : ', ';
+            $use .= $traitName . $quot;
+            $traitName = self::convertName($traitName);
+            $require .= "require_once '" . $traitBasePath . $traitName . ".php';\n";
+        }
+
+
+
+        $result = "<?php\n\n" . $require . "\nclass " . $resultClassName . "\n{\n\t" . $use . "\n}";
+
+        echo "\n\n" . $result . "\n\n";
+
+        file_put_contents($resultClassName . '.php', $result);
+    }
+
+    /**
+     * @param string $name
+     * @param string $from
+     * @param string $to
+     * @return string
+     */
+    public static function convertName($name = '', $from = '_', $to = '/')
+    {
+        return (strpos($name, $from) !== false) ? str_replace($from, $to, $name) : $name;
+    }
 }
