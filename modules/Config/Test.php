b9e2ef92427a2a3da184f2ae1f0d87249d7055c0
Se# <support@funcraft.ru>
2013-11-13 15:31:30 +0300

    Update Config_Test

--- modules/Config/Test.php
+++ modules/Config/Test.php
@@ -2,9 +2,14 @@
 
 trait Config_Test
 {
-  public function testApplyConfig($data)
-  {
-    if(isset($data['skipTests']))
-      $this->skipTests = $data['skipTests'];
-  }
+    public $t2tTime = 0;
+
+    public function testApplyConfig($data)
+    {
+        if(isset($data['skipTests']))
+            $this->skipTests = $data['skipTests'];
+
+        if(isset($data['test-to-test time']))
+            $this->t2tTime = $data['test-to-test time'];
+    }
 }
