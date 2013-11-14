be5089fde1223d171815366d633e235ad1f7ac10
Se# <support@funcraft.ru>
2013-11-14 15:36:35 +0300

    Update and rename man.php to example/man.php

--- man.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'modules/lib/Manager.php';
-
-TestPoint_Manager::compile('TestPoint', 'application/configs/onload.json', 'modules/');
-
-require_once 'TestPoint.php';
-
-$tp = new TestPoint('sett', [__DIR__ . '/tests/TestPointTest.php'], true);
