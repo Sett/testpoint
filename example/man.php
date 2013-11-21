8077c378ee6395bab998313dfbf180762136c9da
Se# <support@funcraft.ru>
2013-11-21 13:51:33 +0300

    Update man.php

--- example/man.php
+++ example/man.php
@@ -1,7 +1,7 @@
 <?php
 require_once '../modules/lib/Manager.php';
 
-TestPoint_Manager::compile('TestPoint', '../application/configs/onload.json', 'modules/');
+TestPoint_Manager::compile('TestPoint', '../application/configs/onload.json');
 
 require_once 'TestPoint.php';
 
