7b141c6076ed92928f655d7cde5179242505b71b
Se# <support@funcraft.ru>
2013-11-21 13:51:43 +0300

    Delete tryme.php

--- example/tryme.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once '../TestPoint.php';
-// example test
-$tp = new TestPoint('sett', [__DIR__ . '/../tests/TestPointTest.php'], true);
-$tp->output($tp->say('Finish', 'h1', true));
