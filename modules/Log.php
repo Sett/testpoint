5351a954410605194886d9e22d9ded89aa7bf0e4
Se# <support@funcraft.ru>
2013-11-21 13:12:15 +0300

    Create Log.php

---
+++ modules/Log.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Class Log
+ */
+trait Log
+{
+    /**
+     * @param $output
+     */
+    public function logTestingOutput($output)
+    {
+        if(is_array($output))
+            $output = implode("\n", $output);
+
+        if($this->logExec)
+        {
+            $this->say('Log', 'h2')->say('Logging testing output into ' . $this->colorText($this->execLogFile, 'underline'));
+            file_put_contents($this->execLogFile, "[" . date('Y-m-d H:i:s') . "]\n" . $output . "\n\n");
+        }
+    }
+}
