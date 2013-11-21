5b971d105676ee751ebc1a2e76d6587560bdd6e3
Se# <support@funcraft.ru>
2013-11-21 14:50:58 +0300

    Update Talk.php

--- modules/Talk.php
+++ modules/Talk.php
@@ -4,5 +4,32 @@
 */
 trait Talk
 {
+    /**
+     * @var string
+     */
     public $talk = 'on';
+
+    /**
+     *
+     */
+    public function startTestTalk()
+    {
+        $this->say('Start test(s)', 'h1');
+    }
+
+    /**
+     * @param string $test
+     */
+    public function runTestTalk($test = '')
+    {
+        $this->say($this->colorText('Run ', 'bold') . $test);
+    }
+
+    /**
+     *
+     */
+    public function resultsTalk()
+    {
+        $this->say('Results', 'h2');
+    }
 }
