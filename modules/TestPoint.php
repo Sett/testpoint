e3becee5ff0e82528a3745388a3365ba2acb2ecf
Se# <support@funcraft.ru>
2013-11-21 14:52:55 +0300

    Update TestPoint.php

--- modules/TestPoint.php
+++ modules/TestPoint.php
@@ -8,63 +8,31 @@ trait TestPoint
     public $player = '';
 
     /**
-     * @param string $player
-     * @param array|string $tests tests names or a directory name, where tests are stored
-     * @param bool $logExec
+     * @param string $testPath tests names or a directory name, where tests are stored
      * @param string $onLoadConfig path/to/file
      */
-    public function __construct($player = '', $tests = [], $logExec = false, $onLoadConfig = '')
+    public function __construct($testPath = '', $onLoadConfig = '')
     {
-        $this->player = $player;
-        $this->say('Constructing TestPoint for "' . $player . '"', 'h1');
-
         $config = $onLoadConfig ? $onLoadConfig : __DIR__ . '/../application/configs/onload.json';
 
         $this->applyConfig($config);
-
-        $tests = $this->getTests($tests);
-        $this->logExec = $logExec;
-
-        if($player && count($tests))
-            $this->runMulti($player, $tests);
+        $this->run($testPath);
     }
 
-    public function runMulti($player, $tests)
+    /**
+     * @param $tests
+     */
+    public function run($tests)
     {
-        $this->say('Start test(s)', 'h1')
-            ->say($this->colorText('Run ', 'bold') . $tests);
+        $this->event('start test', [$this->player, $tests])->event('run test', $tests);
 
         $result     = $this->exec($tests);
         $resultLine = $this->getResultLine($result);
 
-        $this->say('Results', 'h2');
-        $this->log($player, $this->analyse($resultLine));
+        $this->event('results', $resultLine);
+        $this->log($this->player, $this->analyse($resultLine));
+
         $this->event('testing output', $result)->event('the end');
-    }
-
-    /**
-     * @param string $player
-     * @param array $tests
-     */
-    public function run($player, $tests)
-    {
-        $this->say('Start test(s)', 'h1');
-        $logData = [];
-
-        foreach($tests as $test)
-        {
-            $this->say($this->colorText('Run ', 'bold') . $test);
-
-            $result  = $this->exec($test);
-            $logData = array_merge($logData, $result);
-
-            $resultLine  = $this->getResultLine($result);
-
-            $this->say('Results', 'h2');
-            $this->log($player, $this->analyse($resultLine));
-        }
-
-        $this->event('testing output', $logData)->event('the end');
     }
 
     /**
