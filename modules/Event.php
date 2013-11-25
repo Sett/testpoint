7551cd976aa732d7ef6c2979555ccdd178e29c36
Se# <support@funcraft.ru>
2013-11-25 14:08:28 +0300

    Update Event.php

--- modules/Event.php
+++ modules/Event.php
@@ -11,6 +11,12 @@ trait Event
     public $eventListener = [];
 
     /**
+     * event => mixed result
+     * @var array
+     */
+    public $eventResult = [];
+
+    /**
      * @param string $name
      * @param mixed $context
      * @return $this
@@ -22,11 +28,20 @@ trait Event
             foreach($this->eventListener[$name] as $listener)
             {
                 if(method_exists($this, $listener))
-                    $this->$listener($context);
+                    $this->eventResult[$name] = $this->$listener($context);
             }
         }
 
         return $this;
+    }
+
+    /**
+     * @param string $event
+     * @return string
+     */
+    public function getEventResult($event = '')
+    {
+        return isset($this->eventResult[$event]) ? $this->eventResult[$event] : '';
     }
 
     /**
