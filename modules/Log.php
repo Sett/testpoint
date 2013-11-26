735e5148ef087a13347db2d3eac64f012c7d2f77
Se# <support@funcraft.ru>
2013-11-26 14:16:55 +0300

    Update Log.php

--- modules/Log.php
+++ modules/Log.php
@@ -16,7 +16,7 @@ trait Log
         if($this->logExec)
         {
             $this->say('Log', 'h2')->say('Logging testing output into ' . $this->colorText($this->execLogFile, 'underline'));
-            file_put_contents($this->execLogFile, "[" . date('Y-m-d H:i:s') . "]\n" . $output . "\n\n");
+            file_put_contents(__DIR__ . '/../' .$this->execLogFile, "[" . date('Y-m-d H:i:s') . "]\n" . $output . "\n\n");
         }
     }
     
