3e4497b9e626c242947e8b5e0f622450c2033737
Se# <support@funcraft.ru>
2013-12-03 12:20:04 +0300

    Update TestPoint.php

--- modules/TestPoint.php
+++ modules/TestPoint.php
@@ -3,29 +3,11 @@
 trait TestPoint
 {
     /**
-     * @var string
-     */
-    public $player = '';
-
-    /**
      * @param string $testPath tests names or a directory name, where tests are stored
      * @param string $onLoadConfig path/to/file
      */
     public function __construct($testPath = '')
     {
         $this->event('on load', $testPath);
-    }
-
-    /**
-     * @param $tests
-     */
-    public function run($tests)
-    {
-        $this->event('start test', [$this->player, $tests])
-             ->event('run test', $tests)
-             ->event('test result', $this->getEventResult('run test'))
-             ->event('results', $this->getEventResult('test result'))
-             ->event('testing output', $this->getEventResult('run test'))
-             ->event('the end');
     }
 }
