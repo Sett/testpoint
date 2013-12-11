00cf9e33b3bc3f0cb0a6fabfb35d95cecff79711
Se# <support@funcraft.ru>
2013-12-11 11:55:10 +0300

    Update Error.php

--- modules/Error.php
+++ modules/Error.php
@@ -4,6 +4,8 @@ trait Error
 {
     public $errors = [];
     
+    public $errorLevels = ['debug' => 0, 'notice' => 1, 'warning' => 2, 'fatal' => 3];
+    
     public function addError($message = '', $context = null, $level = 'notice')
     {
         $this->errors[] = [
